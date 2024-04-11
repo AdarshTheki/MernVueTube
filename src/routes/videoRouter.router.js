d9a1f0f28b311101a7a3739ff4dd2eb56ae1b47a
adarshtheki <adarshverma549@gmail.com>
2024-04-11 10:17:35 +0530

    Update video router and controller with new endpoints and refactored code

--- src/routes/videoRouter.router.js
+++ src/routes/videoRouter.router.js
@@ -1,19 +1,36 @@
 import { Router } from "express";
-import { publishVideo } from "../controllers/videoRouter.controller.js";
+import {
+    deleteVideo,
+    getAllVideos,
+    getVideoById,
+    publishVideo,
+    togglePublishStatus,
+    updateVideo,
+} from "../controllers/videoRouter.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-// router.use(verifyJWT);
+router.use(verifyJWT);
 
-router.route("/").post(
-    verifyJWT,
-    upload.fields([
-        { name: "videoFile", maxCount: 1 },
-        { name: "thumbnail", maxCount: 1 },
-    ]),
-    publishVideo
-);
+router
+    .route("/")
+    .get(getAllVideos)
+    .post(
+        upload.fields([
+            { name: "videoFile", maxCount: 1 },
+            { name: "thumbnail", maxCount: 1 },
+        ]),
+        publishVideo
+    );
+
+router
+    .route("/:videoId")
+    .get(getVideoById)
+    .delete(deleteVideo)
+    .patch(upload.single("thumbnail"), updateVideo);
+
+router.route("/toggle/publish/:videoId").patch(togglePublishStatus);
 
 export default router;
